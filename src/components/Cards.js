a2f6355d20bcf142d8e36f5cb474271e23b02bc4
Alexandru Becher <becheralexandru@gmail.com>
2022-04-03 15:23:19 +0300

    First code update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,26 @@
 import {useState} from 'react'
 import Card from './Card'
 
-
 function Cards(){
+    const axbecherLink = 'https://raw.githubusercontent.com/axbecher/memory-game/main/public';    
+    
     const [items, setItems] = useState([
-        {id: 1, img: '/img/epicGames.png', stat: ""},
-        {id: 1, img: '/img/epicGames.png', stat: ""},
-        {id: 2, img: '/img/killHouseGames.png', stat: ""},
-        {id: 2, img: '/img/killHouseGames.png', stat: ""},
-        {id: 3, img: '/img/mojang.png', stat: ""},
-        {id: 3, img: '/img/mojang.png', stat: ""},
-        {id: 4, img: '/img/riot-games.png', stat: ""},
-        {id: 4, img: '/img/riot-games.png', stat: ""},
-        {id: 5, img: '/img/rockstarGames.png', stat: ""},
-        {id: 5, img: '/img/rockstarGames.png', stat: ""},
-        {id: 6, img: '/img/saberInteractive.png', stat: ""},
-        {id: 6, img: '/img/saberInteractive.png', stat: ""},
-        {id: 7, img: '/img/steam.png', stat: ""},
-        {id: 7, img: '/img/steam.png', stat: ""},
-        {id: 8, img: '/img/ubisoftToronto.png', stat: ""},
-        {id: 8, img: '/img/ubisoftToronto.png', stat: ""}
+        {id: 1, img: axbecherLink +'/img/epicGames.png', stat: ""},
+        {id: 1, img: axbecherLink +'/img/epicGames.png', stat: ""},
+        {id: 2, img: axbecherLink +'/img/killHouseGames.png', stat: ""},
+        {id: 2, img: axbecherLink +'/img/killHouseGames.png', stat: ""},
+        {id: 3, img: axbecherLink +'/img/mojang.png', stat: ""},
+        {id: 3, img: axbecherLink +'/img/mojang.png', stat: ""},
+        {id: 4, img: axbecherLink +'/img/riot-games.png', stat: ""},
+        {id: 4, img: axbecherLink +'/img/riot-games.png', stat: ""},
+        {id: 5, img: axbecherLink +'/img/rockstarGames.png', stat: ""},
+        {id: 5, img: axbecherLink +'/img/rockstarGames.png', stat: ""},
+        {id: 6, img: axbecherLink +'/img/saberInteractive.png', stat: ""},
+        {id: 6, img: axbecherLink +'/img/saberInteractive.png', stat: ""},
+        {id: 7, img: axbecherLink +'/img/steam.png', stat: ""},
+        {id: 7, img: axbecherLink +'/img/steam.png', stat: ""},
+        {id: 8, img: axbecherLink +'/img/ubisoftToronto.png', stat: ""},
+        {id: 8, img: axbecherLink +'/img/ubisoftToronto.png', stat: ""}
     ].sort(() => Math.random()-0.5))
     
 
@@ -55,9 +56,9 @@ function Cards(){
     }   
 
     return (
-        <div className="col-md-12 w-75 ">
-            <div className="content-center">
-            <div className="container1 container ">
+        <div className="">
+            <div className="">
+            <div className="container">
                 {items.map((item, index) => (
                         <Card key = {index} item={item} id={index} handleClick={handleClick} />
                     ))
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Dropdown} from 'react-bootstrap';
 function Header(){
-    
+    const axbecherLink = 'https://raw.githubusercontent.com/axbecher/memory-game/main/public'; 
 
     return (
 
         <div className="myHeader btn-group">
-
+        
         <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-basic">
                 Axbecher.com
@@ -23,14 +23,14 @@ function Header(){
                 Memory Game Info
             </Dropdown.Toggle>
             <Dropdown.Menu>
-                <Dropdown.Item target="_blank" href="https://www.epicgames.com/store/en-US/">  <img src="./img/epicGames.png" alt="Epic Games"></img> Epic Games</Dropdown.Item>
-                <Dropdown.Item href="https://inthekillhouse.com/"> <img src="./img/killHouseGames.png" alt="KillHouse Games"></img> KillHouse Games</Dropdown.Item>
-                <Dropdown.Item href="https://www.minecraft.net/en-us"> <img src="./img/mojang.png" alt="Mojang"></img> Mojang</Dropdown.Item>
-                <Dropdown.Item href="https://www.riotgames.com/en"> <img src="./img/riot-games.png" alt="Riot Games"></img> Riot Games</Dropdown.Item>
-                <Dropdown.Item href="https://www.rockstargames.com/"> <img src="./img/rockstarGames.png" alt="Rockstar Games"></img> Rockstar Games</Dropdown.Item>
-                <Dropdown.Item href="https://twitter.com/tweetssaber"> <img src="./img/saberInteractive.png" alt="Saber Interactive"></img> Saber Interactive</Dropdown.Item>
-                <Dropdown.Item href="https://store.steampowered.com/"> <img src="./img/steam.png" alt="Steam"></img> Steam</Dropdown.Item>
-                <Dropdown.Item href="https://www.ubisoft.com/en-gb/"> <img src="./img/ubisoftToronto.png" alt="Ubisoft Toronto"></img> Ubisoft Toronto</Dropdown.Item>
+                <Dropdown.Item target="_blank" href="https://www.epicgames.com/store/en-US/">  <img src={axbecherLink+"/img/epicGames.png"} alt="Epic Games"></img> Epic Games</Dropdown.Item>
+                <Dropdown.Item href="https://inthekillhouse.com/"> <img src={axbecherLink+"/img/killHouseGames.png"} alt="KillHouse Games"></img> KillHouse Games</Dropdown.Item>
+                <Dropdown.Item href="https://www.minecraft.net/en-us"> <img src={axbecherLink+"/img/mojang.png"} alt="Mojang"></img> Mojang</Dropdown.Item>
+                <Dropdown.Item href="https://www.riotgames.com/en"> <img src={axbecherLink+"/img/riot-games.png"} alt="Riot Games"></img> Riot Games</Dropdown.Item>
+                <Dropdown.Item href="https://www.rockstargames.com/"> <img src={axbecherLink+"/img/rockstarGames.png"} alt="Rockstar Games"></img> Rockstar Games</Dropdown.Item>
+                <Dropdown.Item href="https://twitter.com/tweetssaber"> <img src={axbecherLink+"/img/saberInteractive.png"} alt="Saber Interactive"></img> Saber Interactive</Dropdown.Item>
+                <Dropdown.Item href="https://store.steampowered.com/"> <img src={axbecherLink+"/img/steam.png"} alt="Steam"></img> Steam</Dropdown.Item>
+                <Dropdown.Item href="https://www.ubisoft.com/en-gb/"> <img src={axbecherLink+"/img/ubisoftToronto.png"} alt="Ubisoft Toronto"></img> Ubisoft Toronto</Dropdown.Item>
             </Dropdown.Menu>
             </Dropdown>
         </div>
